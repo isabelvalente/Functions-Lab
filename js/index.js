c62cd7322fce262c72b098ec95b0ed611f9bd5eb
Tech-A33 <tech-a33@Tech-A33s-MacBook-Air.local>
2018-05-29 15:06:48 +0100

    Completed Functions Lab

--- js/index.js
+++ js/index.js
@@ -1,5 +1,45 @@
+function addTwo(num1, num2){
+  return num1 + num2;
+}
 
+function addThree(num1, num2, num3){
+  return num1 + num2 + num3;
+}
 
+function multiplyTwo(num1, num2){
+  return num1 * num2;
+}
 
+function reverseWord(word){
+  return word.split("").reverse().join("");
+}
+
+function increaseByPercentage(perc, inc){
+  return perc + perc * (inc/100);
+
+}
+
+function celciusToFahrenheit(temp){
+  return temp * 1.8 + 32;
+}
+
+function fahrenheitToCelcius(temp){
+  return (temp - 32) * 5 / 9;
+}
+
+function stripVowels(vowels){
+  return vowels.replace(/[aeiou]/ig,'');
+}
 // do not delete
 runTests();
+
+
+
+
+Array.prototype.demo = function(num1,num2) {
+  return num1 + num2;
+};
+
+var arr = [];
+
+console.log(arr.demo(4,2));
